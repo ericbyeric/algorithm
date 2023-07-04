886a58c59b7a4bf7b6e7ab42735e252feb6a591c
ericbyeric <khsoo9312@gmail.com>
2023-07-04 13:35:47 +0900

    leetcode 530 on 7/4/2023

--- daily_algo/src/main/java/leetcode/day001/GenerateParentheses.java
+++ daily_algo/src/main/java/leetcode/day001/GenerateParentheses.java
@@ -1,4 +1,4 @@
-package leetcode;
+package leetcode.day001;
 
 import java.util.ArrayList;
 import java.util.List;
---
+++ daily_algo/src/main/java/leetcode/day002/MinimumAbsolutDifferenceInBST.java
@@ -0,0 +1,34 @@
+package leetcode.day002;
+
+
+public class MinimumAbsolutDifferenceInBST {
+    // 트리 순회 (재귀)
+    // preorder : self left right
+    // inorder : left self right
+    // postorder : left right self
+    // 이진검색트리 성질 -> inorder -> 오름차순 정렬이 됨!
+    boolean init;
+    int prev;
+    int min;
+    public static int getMinimumDifference(TreeNode root){
+        init = false;
+        int min = Integer.MAX_VALUE;
+        inorder(root);
+        return min;
+    }
+
+    public void inorder(TreeNode root){
+        // 예외 처리
+        if(root==null) return;
+
+        inorder(root.left);
+        //self처리
+        if(!init){
+            init = true;
+        }else{
+            min = Math.min(min,root.val - min);
+        }
+        prev = root.val;
+        inorder(root.right);
+    }
+}
